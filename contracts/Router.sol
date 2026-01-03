aa22d17343f5e10683cc08868f52c00edbe1f809
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 20:59:50 +0600

    Improve Tempo-Swap contract & logic

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -508,7 +508,6 @@ library UniswapV2Library {
         amounts = new uint[](path.length);
         amounts[amounts.length - 1] = amountOut;
         for (uint i = path.length - 1; i > 0; i--) {
-            (uint reserveIn, uint reserveOut) = getReserves(factory, path[i - 1], path[i]);
 
 
     

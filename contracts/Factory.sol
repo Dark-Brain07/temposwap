cd636b6db0d248645b9467a9a1d5df0dc09256d7
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:13:35 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -368,7 +368,6 @@ contract UniswapV2Factory is IUniswapV2Factory {
         bytes memory bytecode = type(UniswapV2Pair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
-            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         
 
 

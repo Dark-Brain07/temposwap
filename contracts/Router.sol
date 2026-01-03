878f3c03f85c42ce7346cffd56911f90147b0567
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 21:17:11 +0600

    Improve Tempo-Swap contract & logic

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -533,3 +533,4 @@ library TransferHelper {
     function safeTransferETH(address to, uint value) internal {
         (bool success,) = to.call{value:value}(new bytes(0));
         require(success, 'TransferHelper: ETH_TRANSFER_FAILED');
+    }

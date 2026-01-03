a0ec59a0a0367f2d2cc2c8c527086a653e89e550
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 21:16:47 +0600

    Improve Tempo-Swap contract & logic

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -528,5 +528,5 @@ library TransferHelper {
     function safeTransferFrom(address token, address from, address to, uint value) internal {
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FROM_FAILED');
-    
+    }
 

c00f59b1b17e8848223bc43456fba76cb38c1fb4
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:37:24 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -370,6 +370,7 @@ contract UniswapV2Factory is IUniswapV2Factory {
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
+        IUniswapV2Pair(pair).initialize(token0, token1);
 
 
     

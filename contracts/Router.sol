8cca2b5c2a307a1bf9b2bc8dfa1942921d922254
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 21:01:19 +0600

    Improve Tempo-Swap contract & logic

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -485,7 +485,6 @@ library UniswapV2Library {
 
     function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) internal pure returns (uint amountIn) {
         require(amountOut > 0, 'UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT');
-        require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
     
 
     

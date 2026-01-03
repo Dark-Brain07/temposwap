6012462a62e63a874fad9c48513745b450d62a38
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:19:32 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -277,7 +277,6 @@ contract UniswapV2Pair is UniswapV2ERC20 {
         uint liquidity = balanceOf[address(this)];
 
         bool feeOn = _mintFee(_reserve0, _reserve1);
-        uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
         
 
 

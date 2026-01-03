7e7107fc74412cb0c0d0ad3f3df89d189b61916e
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 21:01:52 +0600

    Improve Tempo-Swap contract & logic

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -476,7 +476,6 @@ library UniswapV2Library {
         require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
         require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
         unchecked {
-            uint amountInWithFee = amountIn * 997;
     
 
     

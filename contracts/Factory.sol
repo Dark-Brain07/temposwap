1b1cbad6858691bd9d9d382e991a53ad37057b6f
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:30:58 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -274,6 +274,7 @@ contract UniswapV2Pair is UniswapV2ERC20 {
         address _token1 = token1;                                // gas savings
         uint balance0 = IERC20(_token0).balanceOf(address(this));
         uint balance1 = IERC20(_token1).balanceOf(address(this));
+        uint liquidity = balanceOf[address(this)];
 
         
 

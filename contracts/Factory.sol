2ba672d6772ac9ae48a3cc7f12193d8da076414d
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:37:42 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -374,6 +374,7 @@ contract UniswapV2Factory is IUniswapV2Factory {
         getPair[token0][token1] = pair;
         getPair[token1][token0] = pair; // populate mapping in the reverse direction
         allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
 
 
     

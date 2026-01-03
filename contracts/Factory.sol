b7b9d5a0d6615bbf4d30345fb6861d78f04cbd92
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:28:49 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -244,6 +244,7 @@ contract UniswapV2Pair is UniswapV2ERC20 {
         uint amount1;
         unchecked {
              amount0 = balance0 - _reserve0;
+             amount1 = balance1 - _reserve1;
 
             
         

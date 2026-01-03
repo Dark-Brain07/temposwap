176471820311fbfa593a9ec6aec42dd2ace708a2
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:16:11 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -328,7 +328,6 @@ contract UniswapV2Pair is UniswapV2ERC20 {
         }
 
         _update(balance0, balance1, _reserve0, _reserve1);
-        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
     }
 
 

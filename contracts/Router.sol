d40b32369e977a504154156a3daf860e5c3f567e
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 21:03:03 +0600

    Improve Tempo-Swap contract & logic

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -460,7 +460,6 @@ library UniswapV2Library {
 
     function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB) {
         (address token0,) = sortTokens(tokenA, tokenB);
-        (uint reserve0, uint reserve1,) = IUniswapV2Pair(pairFor(factory, tokenA, tokenB)).getReserves();
     
 
     

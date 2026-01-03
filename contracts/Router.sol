33db3d85649620fd3ded31bad1641ac528e211f6
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 21:03:45 +0600

    Improve Tempo-Swap contract & logic

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -448,7 +448,6 @@ contract UniswapV2Router02 {
 
 library UniswapV2Library {
     function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
-        require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
     
 
     

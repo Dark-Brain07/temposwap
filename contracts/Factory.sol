5a7f30c0b1c90b39719a45357ac752e6ab8ace5f
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:22:43 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -228,7 +228,6 @@ contract UniswapV2Pair is UniswapV2ERC20 {
                         uint liquidity = numerator / denominator;
                         if (liquidity > 0) _mint(feeTo, liquidity);
                     }
-                }
             
         
     

207e3488b32f3deec1eb15dc1a65d044e16d4487
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:38:25 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -383,6 +383,7 @@ contract UniswapV2Factory is IUniswapV2Factory {
     }
 
     function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
     }
 
 

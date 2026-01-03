165d039e6ddf9ffc0f31d7c83b2de8c575c32db6
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:16:53 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -318,7 +318,6 @@ contract UniswapV2Pair is UniswapV2ERC20 {
             amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
             amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
         }
-        require(amount0In > 0 || amount1In > 0, 'UniswapV2: INSUFFICIENT_INPUT_AMOUNT');
         
         
 

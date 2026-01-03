b9e01cd76475d9b40f94667797f28cc5814fcda9
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:15:23 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -338,7 +338,6 @@ contract UniswapV2Pair is UniswapV2ERC20 {
             _safeTransfer(_token0, to, IERC20(_token0).balanceOf(address(this)) - reserve0);
             _safeTransfer(_token1, to, IERC20(_token1).balanceOf(address(this)) - reserve1);
         }
-    }
 
     
 

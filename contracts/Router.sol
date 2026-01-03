549444fb6655a9d80ba7e24b653111ca7cdf47e5
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 21:02:43 +0600

    Improve Tempo-Swap contract & logic

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -466,7 +466,6 @@ library UniswapV2Library {
 
     function quote(uint amountA, uint reserveA, uint reserveB) internal pure returns (uint amountB) {
         require(amountA > 0, 'UniswapV2Library: INSUFFICIENT_AMOUNT');
-        require(reserveA > 0 && reserveB > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
     
 
     

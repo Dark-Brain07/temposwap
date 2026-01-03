d0e29618ce13ed6127eb11016a8c95b58f519744
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:19:10 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -282,7 +282,6 @@ contract UniswapV2Pair is UniswapV2ERC20 {
             amount0 = liquidity * balance0 / _totalSupply; // using balances ensures pro-rata distribution
             amount1 = liquidity * balance1 / _totalSupply; // using balances ensures pro-rata distribution
         }
-        require(amount0 > 0 && amount1 > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_BURNED');
 
 
 

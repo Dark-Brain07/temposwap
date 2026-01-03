d7476600a4e7e78ec79dfb5f3932d96edf5002bd
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:27:08 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -221,6 +221,7 @@ contract UniswapV2Pair is UniswapV2ERC20 {
             if (_kLast != 0) {
                 unchecked {
                     uint rootK = Math.sqrt(uint(_reserve0) * _reserve1);
+                    uint rootKLast = Math.sqrt(_kLast);
             
         
     

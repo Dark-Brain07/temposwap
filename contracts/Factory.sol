238c96df1f95f81db3592d9c96a603b0ad5ad0a8
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:20:40 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -258,7 +258,7 @@ contract UniswapV2Pair is UniswapV2ERC20 {
             unchecked {
                 liquidity = Math.min(amount0 * _totalSupply / _reserve0, amount1 * _totalSupply / _reserve1);
             }
-        }
+        
 
 
 

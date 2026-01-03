267f65b9c72af13579eb04b0d44f589d7b785a93
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 21:07:03 +0600

    Improve Tempo-Swap contract & logic

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -398,7 +398,6 @@ contract UniswapV2Router02 {
             path[0], msg.sender, UniswapV2Library.pairFor(factory, path[0], path[1]), amountIn
         );
         _swapSupportingFeeOnTransferTokens(path, address(this));
-        uint amountOutput = IERC20(WETH).balanceOf(address(this));
 
     
 

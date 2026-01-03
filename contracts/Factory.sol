9ba778662a99a7be8aedfe0571d5166e2967158a
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:14:38 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -352,7 +352,6 @@ contract UniswapV2Factory is IUniswapV2Factory {
     mapping(address => mapping(address => address)) public getPair;
     address[] public allPairs;
 
-    constructor(address _feeToSetter) {
     
 
 

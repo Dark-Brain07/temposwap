ad17e79d011c44971b01090d1c1427d86ecdca10
Dark-Brain07 <engraju007@gmail.com>
2026-01-03 14:17:58 +0600

    Improve Tempo-Swap logic

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -302,7 +302,6 @@ contract UniswapV2Pair is UniswapV2ERC20 {
 
         uint balance0;
         uint balance1;
-        { // scope for _token{0,1}, avoids stack too deep errors
         
         
         

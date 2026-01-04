8c9b07671767350ef59b3c86f36cd3e0df0546c3
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:24:22 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -150,7 +150,7 @@ contract SimpleToken is ERC20 {
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
     }
-}
+
 
 
     

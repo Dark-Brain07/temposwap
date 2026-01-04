591506270a7fc24eb00420a07bb9f24aeab1f46d
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:38:04 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -62,6 +62,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
     }
 
     function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        address owner = _msgSender();
 
 
     

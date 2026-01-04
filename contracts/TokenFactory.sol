ee806df18ad78bcf846747a8b5fff573b09839ce
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:42:34 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -136,6 +136,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
         uint256 currentAllowance = allowance(owner, spender);
         if (currentAllowance != type(uint256).max) {
             require(currentAllowance >= amount, "ERC20: insufficient allowance");
+            unchecked {
         
     
 

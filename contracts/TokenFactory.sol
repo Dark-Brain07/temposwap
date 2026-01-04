787641e00c7226db6684eac6561e727dbf8296c4
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:26:25 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -118,7 +118,6 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
         require(account != address(0), "ERC20: mint to the zero address");
         _beforeTokenTransfer(address(0), account, amount);
         _totalSupply += amount;
-        unchecked {
 
     
 

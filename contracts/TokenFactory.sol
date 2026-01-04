56bc940abb40bd97f29082f100d91b6d1e05ed2f
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:26:53 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -111,7 +111,6 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
             _balances[to] += amount;
         }
         emit Transfer(from, to, amount);
-        _afterTokenTransfer(from, to, amount);
 
 
     

081b37f5b97bad317270315292626c05cd15f443
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:33:04 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -24,7 +24,6 @@ abstract contract Context {
     function _msgSender() internal view virtual returns (address) {
         return msg.sender;
     }
-    function _msgData() internal view virtual returns (bytes calldata) {
 
 
 

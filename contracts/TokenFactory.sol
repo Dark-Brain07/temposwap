15fedadef504b922a26f67e9fa7fa69b09789797
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:34:19 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.20;
 // OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/ERC20.sol)
 
 interface IERC20 {
-    event Transfer(address indexed from, address indexed to, uint256 value);
 
 
 

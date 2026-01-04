787aa2729dc28538ca815e1faeacfe3eca14eff9
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:35:00 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.20;
 
 interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
 
 

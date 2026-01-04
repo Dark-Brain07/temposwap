4337dde6d7e9b776ac5d32982a469922d03a065a
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:32:28 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -33,7 +33,6 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
     uint256 private _totalSupply;
-    string private _name;
 
 
 

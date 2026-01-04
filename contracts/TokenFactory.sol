013f955116dfe4b85bf6b0964a94baf8cfa95b69
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:37:52 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -58,6 +58,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
     }
 
     function balanceOf(address account) public view virtual override returns (uint256) {
+        return _balances[account];
 
 
 

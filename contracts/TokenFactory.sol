862a535e52429df7473fdc9f62be68e2550f17ba
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:28:41 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -97,7 +97,6 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
         unchecked {
             _approve(owner, spender, currentAllowance - subtractedValue);
         }
-        return true;
     
 
 

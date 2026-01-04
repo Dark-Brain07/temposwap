2e80e21f32de34b51cabddd632832f3e5f6184b8
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:35:15 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -9,6 +9,7 @@ interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
 
 
 

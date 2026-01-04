8ce1058a15b021a82e0bb501bf8b67bcb81e532e
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:33:31 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -17,7 +17,6 @@ interface IERC20 {
 interface IERC20Metadata is IERC20 {
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
-    function decimals() external view returns (uint8);
 
 
     

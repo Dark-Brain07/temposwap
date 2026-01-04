93c2751ebd109199b8f68d751af523ef44740eed
Dark-Brain07 <engraju007@gmail.com>
2026-01-04 09:43:48 +0600

    Improve Tempo-Swap contract & logic

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -158,5 +158,6 @@ contract TokenFactory {
     function createToken(string memory name, string memory symbol, uint256 initialSupply) external returns (address) {
         SimpleToken newToken = new SimpleToken(name, symbol, initialSupply);
         emit TokenCreated(address(newToken), name, symbol, initialSupply, msg.sender);
+        return address(newToken);
     
 
